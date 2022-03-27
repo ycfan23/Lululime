26533bbd5a48058df24443fada1bcf0a85831bb7
Yachao Fan <61946174+ycfan23@users.noreply.github.com>
2022-03-26 20:24:59 -0500

    Form skeleton setup done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div>
+      <form>
+        <h1>Discount Code Combiner</h1>
+        <p>Use this to combine your discount codes issued by LuluLime Pre-Loved</p>
+        <input
+          type='text'
+          placeholder='Enter discount code'
+        />
+      </form>
+      <button>
+        Combine
+      </button>
     </div>
-  );
+  )
 }
 
 export default App;
