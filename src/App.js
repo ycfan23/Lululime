fb3b04eb1f517472e1e1130dc830110ea39bf915
Yachao Fan <61946174+ycfan23@users.noreply.github.com>
2022-03-27 10:35:43 -0500

    Add another code button completed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [fields, setFields] = useState([{value: null}]);
+
+  function handleChange (index, event) {
+    const values = [...fields];
+    values[index].value = event.target.value;
+    setFields(values);
+  }
+
+  function handleAdd () {
+    const values = [...fields];
+    values.push({value: null})
+    setFields(values)
+  }
+
   return (
     <div>
       <form>
         <h1>Discount Code Combiner</h1>
         <p>Use this to combine your discount codes issued by LuluLime Pre-Loved</p>
-        <input
-          type='text'
-          placeholder='Enter discount code'
-        />
+
+        {fields.map((field, index) => {
+          return (
+            <input
+              type='text'
+              placeholder='Enter discount code'
+              value={field.value || ''}
+              onChange={e => handleChange(e)}
+            />
+          )
+        })}
+
+        <button type='button' onClick={() => handleAdd()}>
+          Add another code
+        </button>
       </form>
-      <button>
-        Combine
-      </button>
     </div>
   )
 }
